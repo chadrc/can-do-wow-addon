8aa8ec387b8d93dd23f00a8c93b2b3a6923d7220
Chad Collins <chadrcollins91@gmail.com>
2020-09-24 07:20:51 -0400

    Added create button and create panel

--- CanDoEditorFrame.lua
+++ CanDoEditorFrame.lua
@@ -1,5 +1,6 @@
 
 function CanDoEditor_Init(editor)
+    editor.currentPanel = editor.displayOptionsPanel;
     CanDo_Print(editor.displayToggleTab:Deactivate())
     tinsert(UISpecialFrames, editor:GetName());
 
@@ -11,12 +12,13 @@ function CanDoEditor_Init(editor)
     end);
 
     function editor:Open(data)
+        local prevButton;
         if table.getn(data) > 0 then 
-
             local function CreateButton(d) 
                 local button = CreateFrame(
                     "Button", 
-                    editor.framesList:GetName() .. "FrameButton" .. d.name, editor.framesList, 
+                    editor.framesList:GetName() .. "FrameButton" .. d.name, 
+                    editor.framesList, 
                     "CanDoEditorFrameListItemButton"
                 );
                 button:SetText(d.name);
@@ -26,11 +28,19 @@ function CanDoEditor_Init(editor)
                     button:GetHighlightTexture():SetVertexColor(1.0, 1.0, 0);
                     button:LockHighlight();
                     CanDoEditorUpdateDisplayPanel(editor, button.data);
+
+                    editor.currentPanel:Show();
+                    editor.createPanel:Hide();
+
+                    if editor.currentButton then
+                        editor.currentButton:UnlockHighlight();
+                    end
+                    editor.currentButton = button;
                 end)
                 return button;
             end
 
-            local prevButton = CreateButton(data[1]);
+            prevButton = CreateButton(data[1]);
 
             for i=2,table.getn(data) do
                 local button = CreateButton(data[i]);
@@ -40,11 +50,41 @@ function CanDoEditor_Init(editor)
             end
         end
 
+        local button = CreateFrame(
+            "Button", 
+            editor.framesList:GetName() .. "FrameButtonCreate",
+            editor.framesList, 
+            "CanDoEditorFrameListItemButton"
+        );
+        button:SetText("Create");
+
+        button:RegisterForClicks("LeftButtonUp", "RightButtonUp");
+        button:SetScript("OnClick", function ()
+            CanDo_Print("Create");
+            button:GetHighlightTexture():SetVertexColor(1.0, 1.0, 0);
+            button:LockHighlight();
+
+            editor.currentPanel:Hide();
+            editor.createPanel:Show();
+
+            if editor.currentButton then
+                editor.currentButton:UnlockHighlight();
+            end
+            editor.currentButton = button;
+        end)
+
+        if prevButton then
+            button:ClearAllPoints(); 
+            button:SetPoint("TOP", prevButton, "BOTTOM", 0, -5);
+        end
+
         editor:Show();
     end
 end
 
 function CanDoEditorOnDisplayTabClicked(editor)
+    editor.currentPanel = editor.displayOptionsPanel;
+
     editor.itemsOptionsPanel:Hide();
     editor.itemsToggleTab:Activate();
 
@@ -52,6 +92,8 @@ function CanDoEditorOnDisplayTabClicked(editor)
 end
 
 function CanDoEditorOnItemsTabClicked(editor)
+    editor.currentPanel = editor.itemsOptionsPanel;
+
     editor.itemsOptionsPanel:Show();
 
     editor.displayOptionsPanel:Hide();
